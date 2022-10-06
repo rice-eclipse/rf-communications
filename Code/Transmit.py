239e41e282e6bebbc35c9deca4651a3f9113442f
Real_Human_1000 <77997353+Real-Human-1000@users.noreply.github.com>
2022-10-05 20:33:44 -0500

    Update Transmit.py
    Examined the code and re-wrote a little bit. Next steps: look at the bitrate budget file and format a message to include all necessary data

--- Code/Transmit.py
+++ Code/Transmit.py
@@ -1,50 +1,45 @@
-
-
 # Simple demo of sending and recieving data with the RFM95 LoRa radio.
-# Author: Tony DiCola
-import time
+# much from https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/circuitpython-for-rfm9x-lora
 
+# initialize SPI connection with radio
 import board
 import busio
 import digitalio
 
-import adafruit_rfm9x
+RADIO_FREQ_MHZ = 433.0
 
+# Initialize SPI
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-# Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
-
-# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
-CS = digitalio.DigitalInOut(board.D5)
-RESET = digitalio.DigitalInOut(board.D6)
-# Or uncomment and instead use these if using a Feather M0 RFM9x board and the appropriate
-# CircuitPython build:
-# CS = digitalio.DigitalInOut(board.RFM9X_CS)
-# RESET = digitalio.DigitalInOut(board.RFM9X_RST)
+# Define CS and RST pins connected to the radio
+cs = digitalio.DigitalInOut(board.D5)
+reset = digitalio.DigitalInOut(board.D6)
 
 # Define the onboard LED
 LED = digitalio.DigitalInOut(board.D13)
 LED.direction = digitalio.Direction.OUTPUT
 
-# Initialize SPI bus.
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+# Create an instance of RFM9x
+import adafruit_rfm9x
+rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, RADIO_FREQ_MHZ)
+# Optional parameter baudrate
+# Default baud rate is 10MHz but that may be too fast
+# If issues arise, decrease to 1MHz
 
-# Initialze RFM radio
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-
-# Note that the radio is configured in LoRa mode so you can't control sync
-# word, encryption, frequency deviation, or other settings!
-
-# You can however adjust the transmit power (in dB).  The default is 13 dB but
-# high power radios like the RFM95 can go up to 23 dB:
+# Adjust transmitting power (dB)
 rfm9x.tx_power = 23
 
-# Send a packet.  Note you can only send a packet up to 252 bytes in length.
-# This is a limitation of the radio packet size, so if you need to send larger
-# amounts of data you will need to break it into smaller send calls.  Each send
-# call will wait for the previous one to finish before continuing.
-While True:
-    rfm9x.send(bytes("Hello world!\r\n", "utf-8"))
-    print("Sent Hello World message!")
-    time.sleep(1)
+
+while True:
+
+    # TODO Replace sample input with proper building of message based on sensor info
+    # Sample input
+    message_to_send = input("Message to send: ")
+    message_bytes = bytes(message_to_send, "utf-8")  # You could save some bytes by only using ASCII
+
+    # For the actual project:
+    # Bitrate Budget https://docs.google.com/spreadsheets/d/1BNU0LOl0tzaBlsRqHiAFNp9Y_h9E01Kwud-uezHMNdA/edit#gid=1938337728
+    # Must append required headers and then include all required information from sensors
+
+    # To send a message, call send()
+    rfm9x.send(message_bytes)
