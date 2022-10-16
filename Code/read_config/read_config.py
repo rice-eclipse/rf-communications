c4d972c089e51d3bb1fee137f27b330d9d0893f6
The King in Yellow <jeavecillat@gmail.com>
2022-10-16 18:49:52 -0500

    Changed the format from 4 tabs of indentation to 4 spaces in the yaml file

--- Code/read_config/read_config.py
+++ Code/read_config/read_config.py
@@ -3,9 +3,10 @@ import yaml
 def read_config(file):
     with open(file, "r") as stream:
         try:
-            print(yaml.safe_load(stream))
+            return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
 
-read_config("test.yaml")
+config = read_config("test.yaml")
+print(config)
