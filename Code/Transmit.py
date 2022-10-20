79c1f4534debd1e675f6bd898841d705433711e9
real-human-1000 <1henryprendergast@gmail.com>
2022-10-19 21:11:49 -0500

    Changed send_to_ground() to be send() and made it take arguments instead of reading from a file. Also turned Transmit into a class

--- Code/Transmit.py
+++ Code/Transmit.py
@@ -5,110 +5,90 @@
 import board
 import busio
 import digitalio
-import yaml
-
-RADIO_FREQ_MHZ = 433.0
-
-# Initialize SPI
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# Define CS and RST pins connected to the radio
-cs = digitalio.DigitalInOut(board.D5)
-reset = digitalio.DigitalInOut(board.D6)
-
-# Define the onboard LED
-LED = digitalio.DigitalInOut(board.D13)
-LED.direction = digitalio.Direction.OUTPUT
-
-# Create an instance of RFM9x
 import adafruit_rfm9x
-rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, RADIO_FREQ_MHZ)
-# Optional parameter baudrate
-# Default baud rate is 10MHz but that may be too fast
-# If issues arise, decrease to 1MHz
-
-# Adjust transmitting power (dB)
-rfm9x.tx_power = 23
 
 
-def send_to_ground():
-    """
-    Reads a YAML file and sends the information to ground via radio
-    """
+class Transmit():
+    def __init__(self):
 
-    # Bitrate Budget https://docs.google.com/spreadsheets/d/1BNU0LOl0tzaBlsRqHiAFNp9Y_h9E01Kwud-uezHMNdA/edit#gid=1938337728
-    # Must extend required headers and then include all required information from sensors
+        self.RADIO_FREQ_MHZ = 433.0
 
-    filename = "filename.yaml"
+        # Initialize SPI
+        self.spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-    with open(filename) as file:
-        data_from_file = yaml.load(file)
+        # Define CS and RST pins connected to the radio
+        self.cs = digitalio.DigitalInOut(board.D5)
+        self.reset = digitalio.DigitalInOut(board.D6)
 
-    # Should load a dictionary as such:
-    # {
-    # "acceleration"    :   (value1, value2, value3),
-    # "gyro"            :   (value1, value2, value3),
-    # "magnetic"        :   (value1, value2, value3),
-    # "altitude"        :   (value),
-    # "gps"             :   (value1, value2),
-    # "temperature"     :   (value)
-    # }
+        # Define the onboard LED
+        self.LED = digitalio.DigitalInOut(board.D13)
+        self.LED.direction = digitalio.Direction.OUTPUT
 
-    # Every value should be an integer that can be stored in 4 bytes
+        # Create an instance of RFM9x
 
-    # 6-Byte FCC License Callsign - required for every packet
-    callsign = bytes("CLSIGN", 'ASCII')
+        self.rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, RADIO_FREQ_MHZ)
+        # Optional parameter baudrate
+        # Default baud rate is 10MHz but that may be too fast
+        # If issues arise, decrease to 1MHz
 
-    # Byte array object with which to build our signal
-    # It might be faster to build the bytearray all at once,
-    # But building it over many extends is more readable
-    data_bytearray = bytearray()
-    data_bytearray.extend(callsign)
-
-    for accel_val in data_from_file["acceleration"]:
-        data_bytearray.extend(accel_val.to_bytes(4, 'big'))
-
-    for gyro_val in data_from_file["gyro"]:
-        data_bytearray.extend(gyro_val.to_bytes(4, 'big'))
-
-    for mag_val in data_from_file["magnetic"]:
-        data_bytearray.extend(mag_val.to_bytes(4, 'big'))
-
-    for gps_val in data_from_file["gps"]:
-        data_bytearray.extend(gps_val.to_bytes(4, 'big'))
-
-    alt_val = data_from_file["altitude"][0]
-    data_bytearray.extend(alt_val.to_bytes(4, 'big'))
-
-    temp_val = data_from_file["temperature"][0]
-    data_bytearray.extend(temp_val.to_bytes(4, 'big'))
-
-    # To send a message, call send()
-    rfm9x.send(bytes(data_bytearray))
-
-    # Final message is 58 bytes
-    # First 6 bytes is callsign
-    # Rest of message is made of 4-byte numbers organized in this order:
-    # Acceleration value 1, acceleration value 2, acceleration value 3
-    # Gyro value 1, gyro value 2, gyro value 3
-    # Magnetic value 1, magnetic value 2, magnetic value 3
-    # Altitude value
-    # GPS value 1, GPS value 2
-    # Temperature value
-
-    # This order is, of course, completely arbitrary
-
-    # Note that the values are big-endian and will have to be decoded properly
+        # Adjust transmitting power (dB)
+        self.rfm9x.tx_power = 23
 
 
-# Testing - Must change function to accept an argument
-# test_data = {
-#     "acceleration"  :   (0, 1, 2),
-#     "gyro"          :   (10, 11, 12),
-#     "magnetic"      :   (20, 21, 22),
-#     "altitude"      :   (30,),
-#     "gps"           :   (40, 41),
-#     "temperature"   :   (50,)
-# }
-#
-# send_to_ground(test_data)
+    def send(self, acceleration, gyro, magnetic, altitude, gps, temperature):
+        """
+        Sends data:
+            acceleration (3-tuple),
+            gyro (3-tuple),
+            magnetic (3-tuple),
+            altitude (float?),
+            gps (2-tuple),
+            temperature (float?)
+        """
+
+        # Bitrate Budget https://docs.google.com/spreadsheets/d/1BNU0LOl0tzaBlsRqHiAFNp9Y_h9E01Kwud-uezHMNdA/edit#gid=1938337728
+        # Must extend required headers and then include all required information from sensors
+
+        # Every value should be an integer that can be stored in 4 bytes
+
+        # 6-Byte FCC License Callsign - required for every packet
+        callsign = bytes("CLSIGN", 'ASCII')
+
+        # Byte array object with which to build our signal
+        # It might be faster to build the bytearray all at once,
+        # But building it over many extends is more readable
+        data_bytearray = bytearray()
+        data_bytearray.extend(callsign)
+
+        for accel_val in acceleration:
+            data_bytearray.extend(accel_val.to_bytes(4, 'big'))
+
+        for gyro_val in gyro:
+            data_bytearray.extend(gyro_val.to_bytes(4, 'big'))
+
+        for mag_val in magnetic:
+            data_bytearray.extend(mag_val.to_bytes(4, 'big'))
+
+        for gps_val in gps:
+            data_bytearray.extend(gps_val.to_bytes(4, 'big'))
+
+        data_bytearray.extend(altitude.to_bytes(4, 'big'))
+
+        data_bytearray.extend(temperature.to_bytes(4, 'big'))
+
+        # To send a message, call send()
+        self.rfm9x.send(bytes(data_bytearray))
+
+        # Final message is 58 bytes
+        # First 6 bytes is callsign
+        # Rest of message is made of 4-byte numbers organized in this order:
+        #   Acceleration value 1, acceleration value 2, acceleration value 3
+        #   Gyro value 1, gyro value 2, gyro value 3
+        #   Magnetic value 1, magnetic value 2, magnetic value 3
+        #   Altitude value
+        #   GPS value 1, GPS value 2
+        #   Temperature value
+
+        # This order is, of course, completely arbitrary
+
+        # Note that the values are big-endian and will have to be decoded properly
