a8b58dc35bef9b28807bf3b3169deac9a904a024
The King in Yellow <jeavecillat@gmail.com>
2022-10-23 17:19:47 -0500

    Added a docstring

--- Code/read_config/read_config.py
+++ Code/read_config/read_config.py
@@ -1,6 +1,19 @@
 import yaml
 
 def read_config(file):
+    """
+    Reads yaml files and attaches its parameters as strings in the keys of a dictionary and its corresponding values are also mapped to them. In case they are nested, we get a nested dictionary as well.
+
+    Finally, it also distinguishes between floats and integers.
+
+    Input:
+        file - path of the yaml file to read
+
+    Output:
+        yaml.safe_load(stream) - dictionary with the properties of our file as keys and its values mapped to it in their correct type.
+
+    """
+
     with open(file, "r") as stream:
         try:
             return yaml.safe_load(stream)
@@ -10,5 +23,5 @@ def read_config(file):
             print(exc)
         
 
-config = read_config("primary_config.yaml")
-print(config)
+#config = read_config("primary_config.yaml")
+#print(config)
