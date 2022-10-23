b01637459b1265b7a01b10d723247dba7087a942
The King in Yellow <jeavecillat@gmail.com>
2022-10-23 16:30:18 -0500

    last config

--- Code/read_config/read_config.py
+++ Code/read_config/read_config.py
@@ -4,9 +4,11 @@ def read_config(file):
     with open(file, "r") as stream:
         try:
             return yaml.safe_load(stream)
+            
+
         except yaml.YAMLError as exc:
             print(exc)
+        
 
-
-config = read_config("test.yaml")
+config = read_config("primary_config.yaml")
 print(config)
