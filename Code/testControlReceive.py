247c2d3eb81f139d3508fb60a3a5d6d863afd70a
real-human-1000 <1henryprendergast@gmail.com>
2022-11-06 16:09:21 -0600

    Deleted unused / superseded code like Transmit.py and Receive.py (replaced by Radio.py)

--- Code/testControlReceive.py
+++
@@ -1,5 +0,0 @@
-from Radio import Radio
-
-radio = Radio()
-
-# Receive tests sent by another device running testControlSend.py and write all to a file
--- Code/testControlSend.py
+++
@@ -1,5 +0,0 @@
-from Radio import Radio
-
-radio = Radio()
-
-# Test several bitrates by sending messages to another device running testControlReceive.py
