dd47231f10f3067aed6d7d58318421e9722fbad9
AdamSher34 <als32@rice.edu>
2023-06-21 16:26:09 -0500

    more changes

--- code/main_rtr_sensor.py
+++ code/main_rtr_sensor.py
@@ -1,28 +1,47 @@
+import os
+
 from sensors.altimeter import Altimeter
 import sensors.gps
-import transceiver.radio_fsk
+# import transceiver.radio_fsk
+import transceiver.radio_lora
 
 
-CONFIG_FILE = 'config/config_rtr.yaml'
+# CONFIG_FILE = 'config/config_rtr.yaml'
+CONFIG_FILE = 'config/config_rtr_lora.yaml'
 
 alt = Altimeter(bus=0)
 gps = sensors.gps.GPS(sensors.gps.GPS_Mode.NORMAL)
-trx = transceiver.radio_fsk.Radio_FSK(CONFIG_FILE)
+# trx = transceiver.radio_fsk.Radio_FSK(CONFIG_FILE)
+trx = transceiver.radio_lora.Radio_LoRa(CONFIG_FILE, None)
 
 while True:
     pressure, temperature = alt.read()
     altitude = Altimeter.altitude(pressure, temperature)
-    print(pressure, temperature, altitude)
     gps.update()
-    print(gps.gps.has_fix, gps.gps.fix_quality, gps.gps.latitude, gps.gps.longitude, gps.time)
+    if gps.time is not None:
+        year, month, day, hour, minute, second = gps.time
+    else:
+        year, month, day, hour, minute, second = None, None, None, None, None, None
 
-    data = {'command': transceiver.radio_fsk.Command.NULL.value,
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"Date: {year}/{month}/{day}\t\t\tTime: {hour}:{minute}:{second} UTC")
+    print(f"Pressure: {pressure} mbar\t\tTemperature: {temperature} {chr(176)}C\t\tAltitude: {altitude} m")
+    print(f"Fix Quality: {gps.gps.fix_quality}\t\t\tSatellites: {gps.gps.satellites}\t\t")
+    print(f"Latitude: {gps.gps.latitude} {chr(176)}N\t\tLongitude: {gps.gps.longitude} {chr(176)}E")
+
+    data = {'command': 0,#transceiver.radio_fsk.Command.NULL.value,
             'pressure': pressure,
             'temperature': temperature,
             'altitude': altitude,
             'latitude': gps.gps.latitude,
             'longitude': gps.gps.longitude,
             'fix_quality': gps.gps.fix_quality,
-            'satellites': gps.gps.satellites}
+            'satellites': gps.gps.satellites,
+            'year': year,
+            'month': month,
+            'day': day,
+            'hour': hour,
+            'minute': minute,
+            'second': second}
 
-    trx.send_and_log(data)
+    trx.send(data)
